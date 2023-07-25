f5436e78cf5ff62f4338f920fe3acb0d6454c201
barisyirtinci <brsyrtnc@gmail.com>
2023-07-25 22:54:22 +0300

    last commit

---
+++ final_project/machinetranslation/tests/test_translator.py
@@ -0,0 +1,19 @@
+""" Test Module """
+import unittest
+
+
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(english_to_french("Hello"),"Bonjour")
+        self.assertNotEqual(english_to_french("Hello World"),"Comment allez-vous")
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(french_to_english("Bonjour"),"Hello")
+        self.assertNotEqual(french_to_english("Quel est ton nom"),"who are you")
+
+unittest.main()
